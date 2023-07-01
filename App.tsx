ee96133e522859ff43209073965c8d452bea3f17
Artyom Nesterenko <code+github@nesterenko.dev>
2023-07-01 15:46:17 +0300

    Add Details screen

--- App.tsx
+++ App.tsx
@@ -4,14 +4,16 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {FeedScreen} from './src/screens/FeedScreen/FeedScreen';
+import {DetailsScreen} from './src/screens/DetailsScreen/DetailsScreen';
 
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Feed">
         <Stack.Screen name="Feed" component={FeedScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/DetailsScreen/DetailsScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from './DetailsScreen.styles';
+
+const DetailsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+};
+
+export {DetailsScreen};
